0d297fb50beffd65c66a7ee8dc92520a6c166e4a
ievas <ieva.antons@gmail.com>
2018-11-07 09:54:49 -0700

    feat: list component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,7 @@
 import React, { Component } from 'react'
-import './Map.js'
-import './LocationList.js'
-import './LocationListItem.js'
-import 'leaflet/dist/leaflet.css'
+import LocationList from './LocationList.js'
+
 import './App.css'
-import L from 'leaflet'
-import * as api from './api.js'
-
-import Modal from 'react-modal';
-Modal.setAppElement('#root');
-
-const customStyles = {
-  content : {
-    top                   : '50%',
-    left                  : '50%',
-    right                 : 'auto',
-    bottom                : 'auto',
-    marginRight           : '-50%',
-    transform             : 'translate(-50%, -50%)'
-  }
-};//modal
-
-delete L.Icon.Default.prototype._getIconUrl;
-
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-  iconUrl: require('leaflet/dist/images/marker-icon.png'),
-  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
-});
-//fix for default icon path; a workaround by @PTihomir from: https://github.com/Leaflet/Leaflet/issues/4968#issuecomment-269750768
 
 let locations = [
   {latlng: {lat: 56.983903, lng: 24.194472}, title: 'Rīgas Krusta baznīca' },
@@ -41,8 +14,6 @@ let locations = [
   {latlng: {lat: 56.948963, lng: 24.089456}, title: 'Saules akmens'}//saules akmens
 ];
 
-let map;
-
 class App extends Component {
 
   state = {
@@ -53,156 +24,11 @@ class App extends Component {
     popupIconUrl: ''
   }
 
-  componentDidMount() {
-
-    map = L.map('mapid').setView([56.970647, 24.157338], 11.5);
-
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-      maxZoom: 18,
-      id: 'mapbox.streets',
-      accessToken: 'pk.eyJ1IjoiamV2YSIsImEiOiJjamx0djlxZWMwZTBhM3FvaXA2a3JteDN5In0.T6cHWQyXjXsDhwBVTemdYw'
-    }).addTo(map);
-
-    locations.forEach(location => {
-      let {latlng} = location;
-      location.marker = L.marker(latlng, {
-        icon:L.icon({
-          iconUrl: 'https://unpkg.com/leaflet@1.0.3/dist/images/marker-icon.png',
-          className: 'blinking'
-        })
-      }).addTo(map);
-
-    location.marker.on('click', async e => {
-
-      let location = locations.find(location => location.marker === e.target);
-
-      if (location.marker.getPopup()){
-        return;
-      }
-      let { title } = location;
-      let {lat, lng} = location.latlng;
-
-      let foursquareData = await api.fetchFoursquareData(lat, lng, title);
-
-      location.marker.unbindPopup();
-      location.marker.bindPopup(`
-          <p>${title}</p>
-          <img src="${foursquareData.iconUrl}" alt="${title}"/>
-        `);
-
-      location.marker.openPopup();
-      });
-    })
-  }
-
-  //blink animation from: https://stackoverflow.com/questions/41884070/how-to-make-markers-in-leaflet-blinking
-  componentWillUnmount(){
-
-  }
-
-  async selectItem(e) {
-
-    let location = locations.find(location => location.title === e.target.innerText);
-    location.marker.openPopup();
-
-    let { title } = location;
-    let {lat, lng} = location.latlng;
-
-    let foursquareData = await api.fetchFoursquareData(lat, lng, title);
-
-    location.marker.unbindPopup();
-
-    location.marker.bindPopup(`
-      <p>${title}</p>
-      <img src="${foursquareData.iconUrl}" alt="${title}"/>
-    `);
-
-    location.marker.openPopup();
-
-    this.setState({
-      modalTitle: title,
-      modalImageUrl: foursquareData.imageUrl,
-     });
-
-    this.openModal();
-  }
-
-  search(e) {
-
-    let found = locations.filter(location => {
-      return RegExp(e.target.value, 'i').test(location.title);
-    });
-
-    this.setState({
-      locations: found
-    });
-//hide markers
-    locations.forEach(location => {
-
-      location.marker.removeFrom(map);
-
-      if (found.find(loc => loc.title === location.title)) {
-        location.marker.addTo(map);
-      }
-
-    });
-  }
-
-  openModal() {
-    this.setState({modalIsOpen: true});
-  }
-
-  afterOpenModal() {
-    // references are now sync'd and can be accessed.
-    // this.subtitle.style.color = '#f00';
-  }
-
-  closeModal() {
-    this.setState({modalIsOpen: false});
-  }
-
   render() {
-
-    let list = this.state.locations.map(location => {
-      return <div onClick={(e)=>{this.selectItem(e)}} className="list-item">{location.title}</div>;
-    });
-
     return (
       <div className="App">
-
-        <Modal
-          isOpen={this.state.modalIsOpen}
-          onAfterOpen={this.afterOpenModal}
-          onRequestClose={()=>this.closeModal()}
-          style={customStyles}
-          contentLabel="Info Modal">
-            <div className="modal-content-wrapper">
-             <h2 ref={subtitle => this.subtitle = subtitle}>{this.state.modalTitle}</h2>
-             <button onClick={()=>this.closeModal()} href="#close" className="close-button">x</button>
-             <img src={this.state.modalImageUrl} alt={this.state.modalTitle}/>
-            </div>
-        </Modal>
-
         <header className="header">Riga Bedtime Story Route<br/><i className="fa fa-bicycle" aria-hidden="true"></i></header>
-
-        <div className="content">
-          <div className="location-container">
-            <div className="input-wrapper">
-              <input onChange={e => this.search(e)}
-              className="list-item"
-              type="text"
-              placeholder="Search"
-              //value={this.state.query}
-              //onChange = {(event) => this.updateQuery(event.target.value)}
-              />
-            </div>
-            <div className="location-list">{list}</div>
-          </div>
-          <div className="map-container">
-            <div id="mapid"></div>
-          </div>
-        </div>
+        <LocationList locations={locations}/>
       </div>
     );
   }
--- src/LocationList.js
+++ src/LocationList.js
@@ -1,11 +1,179 @@
+import * as api from './api.js';
 import React from 'react'
+import Modal from 'react-modal';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet'
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
+// fix for default icon path; a workaround by @PTihomir from: https://github.com/Leaflet/Leaflet/issues/4968#issuecomment-269750768
+
+
+Modal.setAppElement('#root');
+
+const customStyles = {
+  content : {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)'
+  }
+}; // modal styles
 
 class LocationList extends React.Component {
 
-  render(){
-    return(
-      <div></div>
-    )
+  map
+
+  state = {
+    locations: this.props.locations,
+    modalIsOpen: false,
+    modalTitle: '',
+    modalImageUrl: ''
+  }
+
+  componentDidMount() {
+
+    this.map = L.map('mapid').setView([56.970647, 24.157338], 11.5);
+
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox.streets',
+      accessToken: 'pk.eyJ1IjoiamV2YSIsImEiOiJjamx0djlxZWMwZTBhM3FvaXA2a3JteDN5In0.T6cHWQyXjXsDhwBVTemdYw'
+    }).addTo(this.map);
+
+    this.props.locations.forEach(location => {
+      let {latlng} = location;
+      location.marker = L.marker(latlng, {
+        icon:L.icon({
+          iconUrl: 'https://unpkg.com/leaflet@1.0.3/dist/images/marker-icon.png',
+          className: 'blinking'
+        })
+      }).addTo(this.map);
+//blink animation from: https://stackoverflow.com/questions/41884070/how-to-make-markers-in-leaflet-blinking
+    location.marker.on('click', async e => {
+
+      let location = this.props.locations.find(location => location.marker === e.target);
+
+      if (location.marker.getPopup()){
+        return;
+      }
+      let { title } = location;
+      let {lat, lng} = location.latlng;
+
+      let foursquareData = await api.fetchFoursquareData(lat, lng, title);
+
+      location.marker.unbindPopup();
+      location.marker.bindPopup(`
+          <p>${title}</p>
+          <img src="${foursquareData.iconUrl}" alt="${title}"/>
+        `);
+
+      location.marker.openPopup();
+      });
+    })
+  }
+
+  async selectItem(e) {
+
+    let location = this.props.locations.find(location => location.title.toLowerCase() === e.target.innerText.toLowerCase());
+    location.marker.openPopup();
+
+    let { title } = location;
+    let {lat, lng} = location.latlng;
+
+    let foursquareData = await api.fetchFoursquareData(lat, lng, title);
+
+    location.marker.unbindPopup();
+
+    location.marker.bindPopup(`
+      <p>${title}</p>
+      <img src="${foursquareData.iconUrl}" alt="${title}"/>
+    `);
+
+    location.marker.openPopup();
+
+    this.setState({
+      modalTitle: title,
+      modalImageUrl: foursquareData.imageUrl,
+     });
+
+    this.openModal();
+  }
+
+  openModal() {
+    this.setState({modalIsOpen: true});
+  }
+
+  closeModal() {
+    this.setState({modalIsOpen: false});
+  }
+
+  search(e) {
+
+    let found = this.props.locations.filter(location => {
+      return RegExp(e.target.value, 'i').test(location.title);
+    });
+
+    this.setState({
+      locations: found
+    });
+
+    // hide markers
+    this.props.locations.forEach(location => {
+
+      location.marker.removeFrom(this.map);
+
+      if (found.find(loc => loc.title.toLowerCase() === location.title.toLowerCase())) {
+        location.marker.addTo(this.map);
+      }
+
+    });
+  }
+
+  render() {
+
+    let list = this.state.locations.map(location => {
+      return <div onClick={(e)=>{this.selectItem(e)}} className="list-item">{location.title}</div>;
+    });
+
+    return (
+      <React.Fragment>
+        <Modal
+          isOpen={this.state.modalIsOpen}
+          onRequestClose={()=>this.closeModal()}
+          style={customStyles}
+          contentLabel="Info Modal">
+            <div className="modal-content-wrapper">
+             <h2 ref={subtitle => this.subtitle = subtitle}>{this.state.modalTitle}</h2>
+             <button onClick={()=>this.closeModal()} href="#close" className="close-button">x</button>
+             <img src={this.state.modalImageUrl} alt={this.state.modalTitle}/>
+            </div>
+        </Modal>
+        <div className="content">
+          <div className="location-container">
+            <div className="input-wrapper">
+              <input onChange={e => this.search(e)}
+              className="list-item"
+              type="text"
+              placeholder="Search"
+              />
+            </div>
+            <div className="location-list">{list}</div>
+          </div>
+          <div className="map-container">
+            <div id="mapid"></div>
+          </div>
+        </div>
+      </React.Fragment>
+    );
   }
 }
 
